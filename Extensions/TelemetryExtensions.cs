e930d78dce0e9a9e81e8b8ad78111c33cbab0361
riskiekalimuda <riskie.kalimuda@gmail.com>
2026-09-14 22:11:50 +0700

    add telemetry

---
+++ Extensions/TelemetryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace Order.Msv.Extensions
+{
+    public static class TelemetryExtensions
+    {
+        public static IServiceCollection AddOrderTelemetry(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Mengambil nama service secara dinamis atau hardcode khusus untuk Order Service
+            var serviceName = "OrderService";
+
+            services.AddOpenTelemetry()
+                .ConfigureResource(resource => resource.AddService(serviceName))
+                .WithTracing(tracing =>
+                {
+                    tracing
+                        .AddAspNetCoreInstrumentation(options => options.RecordException = true) // Menangkap incoming HTTP request dari YARP
+                        .AddHttpClientInstrumentation(options => options.RecordException = true) // Menangkap jika Order memanggil Purchase via HTTP
+                        .AddOtlpExporter(options =>
+                        {
+                            options.Endpoint = new Uri(configuration["OTEL_EXPORTER_OTLP_ENDPOINT"] ?? "http://localhost:4317");
+                        });
+                });
+
+            return services;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,12 +3,15 @@ using MessageMQCommon.MQ.Names;
 using MessageMQCommon.Parameters;
 using Microsoft.EntityFrameworkCore;
 using Order.Msv.Consumers;
+using Order.Msv.Extensions;
 using Order.Msv.Models;
 using Order.Msv.Profiles;
 using Order.Msv.Services;
 // Pengaturan ini memaksa .NET dan Npgsql menyelaraskan format DateTime lama/lokal menjadi kompatibel dengan pemformatan database
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOrderTelemetry(builder.Configuration);
 
 builder.Services.AddDbContext<OrderMsvDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("OrderMsvDBConnection")));
